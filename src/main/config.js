12fc1c304858ea315695317440332aa3f75926b8
KOSASIH <kosasihg88@gmail.com>
2024-10-31 15:56:23 +0700

    Create config.js

---
+++ src/main/config.js
@@ -0,0 +1,6 @@
+const config = {
+    ethereumNodeUrl: 'https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID', // Replace with your Infura project ID or local node URL
+    networkId: '1', // Mainnet ID; change to '3' for Ropsten, '4' for Rinkeby, etc.
+};
+
+module.exports = config;
